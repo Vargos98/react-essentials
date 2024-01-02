b39e2bb33b333585af427e702a1071d3e1fb5fb9
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-01-02 12:20:05 +0530

    Update Header.jsx
    Added data dynamically also added an auto change description by rendering data dynamically

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import reacImg from './assets/react-core-concepts.png';
+const description = ['Fundamental', 'Crucial','Core'];
+function genRandomInt(max){
+  return Math.floor(Math.random() * (max+1));
+}
+
 
 const Header = () => {
+  const loop = description[genRandomInt(2)];
+ 
+
   return (
     <div>
         <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reacImg} alt="react-atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {loop} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
