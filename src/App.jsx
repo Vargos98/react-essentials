70280aa52f241aa8e793b1070b90daddf0a2297a
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-01-02 12:41:11 +0530

    Created CoreConcepts.jsx and added data.js
    In this I have used props to dynamically render data also used destructuring and spread operator to minimize the code length.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import CoreConcepts from "./CoreConcepts";
 import Header from "./Header";
-
+import { CORE_CONCEPTS } from './data.js';
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts{...CORE_CONCEPTS[1]} />
+            <CoreConcepts{...CORE_CONCEPTS[2]} />
+            <CoreConcepts{...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
+
     </div>
   );
 }
