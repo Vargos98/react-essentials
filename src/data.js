438a5d36670fff86da0edf4fa1f10887e429226b
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-11-13 14:02:53 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -207,7 +207,7 @@ root.render(<App />);
   hooks: {
     title: 'Hooks',
     description:
-      'Hookes were added to React in version 16.8. Hooks allow function components to have access to state and other React features. Because of this, class components are generally no longer needed.',
+      'Hooks were added to React in version 16.8. Hooks allow function components to have access to state and other React features. Because of this, class components are generally no longer needed.',
     code: `
     import React, { useState } from "react";
     import ReactDOM from "react-dom/client";
